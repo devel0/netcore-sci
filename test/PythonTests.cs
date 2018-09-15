698acf5fab4f067bc9f50d1ef3e2d3b0390f2fa8
Lorenzo Delana <oss.devel@searchathing.com>
2018-09-15 16:35:52 +0200

    set python test sympy as SkippableFact to allow skip if platform haven't installed sympy library

--- test/PythonTests.cs
+++ test/PythonTests.cs
@@ -12,10 +12,22 @@ namespace SearchAThing.Sci.Tests
         PythonPipe pipe = null;
         PythonPipe sympyPipe = null;
 
+        bool has_sympy_library = true;
+
         public PythonTests()
         {
             pipe = new PythonPipe();
             sympyPipe = new PythonPipe("from sympy import *", (s) => System.Diagnostics.Debug.WriteLine(s));
+            try
+            {
+                var strwr = new StringWrapper();
+                strwr.str = "x = symbols('x')";
+                sympyPipe.Exec(strwr);
+            }
+            catch
+            {
+                has_sympy_library = false;
+            }
         }
 
         public void Dispose()
@@ -44,14 +56,16 @@ namespace SearchAThing.Sci.Tests
             Assert.True(reslines.Length == 2 && reslines[0] == "3" && reslines[1] == "8");
         }
 
-        [Fact]
+        [SkippableFact]
         public void Test2()
-        {
+        {            
+            Skip.IfNot(has_sympy_library);
+
             var strwr = new StringWrapper();
             strwr.str = @"x = symbols('x')
 print(integrate(x**2 * cos(x), x))
 print(integrate(x**2 * cos(x), (x, 0, pi/2)))";
-            var res = sympyPipe.Exec(strwr, remove_tmp_file: false).str;
+            var res = sympyPipe.Exec(strwr).str;
             var reslines = res.Lines().ToArray();
             Assert.True(reslines.Length == 2 && reslines[0] == "x**2*sin(x) + 2*x*cos(x) - 2*sin(x)" && reslines[1] == "-2 + pi**2/4");
         }
