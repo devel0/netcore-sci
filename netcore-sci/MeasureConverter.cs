e9f28d3fa42e2adf70b10a1eb90fd86615ebffa2
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-06-15 15:24:18 +0200

    write and parse admin measure ad number

--- netcore-sci/MeasureConverter.cs
+++ netcore-sci/MeasureConverter.cs
@@ -12,7 +12,7 @@ using Newtonsoft.Json.Linq;
 
 namespace SearchAThing.Sci
 {
-    
+
     public class MeasureJsonConverter : JsonConverter
     {
         public override bool CanConvert(Type objectType)
@@ -20,21 +20,29 @@ namespace SearchAThing.Sci
             return true;
         }
 
+        static Type doubleTypeof = typeof(double);
+
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             Measure res = null;
 
-            res = Measure.TryParse((string)reader.Value);
+            if (reader.Value.GetType() == doubleTypeof)
+                res = new Measure((double)reader.Value, MUCollection.Adimensional.adim);
+            else
+                res = Measure.TryParse((string)reader.Value);
             if (res == null) return existingValue;
 
-            return res;            
+            return res;
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var measure = (Measure)value;
 
-            writer.WriteValue(measure.ToString(includePQ: true));
+            if (measure.MU == MUCollection.Adimensional.adim)
+                writer.WriteValue(measure.Value);
+            else
+                writer.WriteValue(measure.ToString(includePQ: true));
         }
     }
 
