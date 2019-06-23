4b483fa1d4af8d06f0b0a2020e53bbf39dd22d1c
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-06-23 02:00:51 +0200

    script to analyze using sonarqube

---
+++ analyze-sonarcloud.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+token="$(cat ~/security/sonar/login.txt)"
+exdir="$(dirname `readlink -f "$0"`)"
+cd "$exdir"
+dotnet sonarscanner begin \
+	/k:"devel0_netcore-sci" /n:"devel0_netcore-sci" /v:"1.0.7" /o:"devel0-github" \
+	/d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" \
+	/d:sonar.cs.opencover.reportsPaths="${exdir}/lcov.opencover.xml"
+dotnet restore
+dotnet build
+dotnet test test/test.csproj \
+	/p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
+dotnet sonarscanner end /d:sonar.login="${token}"
