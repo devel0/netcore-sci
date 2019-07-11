28de6b150e4ab38e896669d09a5f6bf7327fd12e
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-07-11 21:46:01 +0200

    cover exclude thirdy

--- analyze-sonarcloud.sh
+++ analyze-sonarcloud.sh
@@ -4,7 +4,8 @@ exdir="$(dirname `readlink -f "$0"`)"
 cd "$exdir"
 dotnet sonarscanner begin \
 	/k:"devel0_netcore-sci" /n:"devel0_netcore-sci" /v:"1.0.10" /o:"devel0-github" \
-	/d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" \
+	/d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" \
+  /d:sonar.exclusions="**/bin/**/*,**/obj/**/*,**/thirdy*/**/*" \
 	/d:sonar.cs.opencover.reportsPaths="${exdir}/lcov.opencover.xml"
 dotnet restore
 dotnet build
