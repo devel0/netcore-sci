d7270856f35d3f3fb4f4fc29714306e317b62cfd
Lorenzo Delana <lorenzo.delana@gmail.com>
2019-07-11 12:53:47 +0200

    cs intersect unit test

--- test/CoordinateSystem3DTests.cs
+++ test/CoordinateSystem3DTests.cs
@@ -98,6 +98,31 @@ namespace SearchAThing.Sci.Tests
             Assert.True(cs2cad.Contains(1e-3, pt) && !cs1cad.Contains(1e-3, pt));
         }
 
+        /// <summary>
+        /// CoordinateSystem3DTest_005.dxf
+        /// </summary>
+        [Fact]
+        public void CoordinateSystem3DTest_005()
+        {
+            var cs1o = new Vector3D("X = -2.67223215 Y = -94.22126913 Z = -293.08306335");
+            var cs1x = new Line3D(cs1o, new Vector3D("X = -221.37919777 Y = 144.86810067 Z = -168.96320246"));
+            var cs1y = new Line3D(cs1o, new Vector3D("X = 192.45964896 Y = 5.43267925 Z = -141.20969062"));
+            var cs1 = new CoordinateSystem3D(cs1o, cs1x.V, cs1y.V);
+
+            var cs2o = new Vector3D("X = -187.42845697 Y = 128.44203407 Z = 25.92565607");
+            var cs2x = new Line3D(cs2o, new Vector3D("X = -9.40307509 Y = 246.81618785 Z = 25.92565607"));
+            var cs2y = new Line3D(cs2o, new Vector3D("X = -187.42845697 Y = 128.44203407 Z = 224.4413331"));
+            var cs2 = new CoordinateSystem3D(cs2o, cs2x.V, cs2y.V);
+
+            var i12Line = cs1.Intersect(1e-4, cs2);
+            var i21Line = cs2.Intersect(1e-4, cs1);
+
+            Assert.True(i12Line.LineContainsPoint(1e-4, i21Line.From));
+            Assert.True(i12Line.LineContainsPoint(1e-4, i21Line.To));
+            Assert.True(i12Line.LineContainsPoint(1e-4, new Vector3D("X = -187.42845697 Y = 128.44203407 Z = -170.87092273")));
+            Assert.True(i12Line.LineContainsPoint(1e-4, new Vector3D("X = 29.37373838 Y = 272.59999706 Z = 25.92565607")));
+        }
+
     }
 
 }
