0877e1ae72c47034c54f0d8a2eeeed7d927d8e27
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-06-26 18:23:37 +0200

    example 0003 testing with simple triangle

---
+++ examples/0003/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace example_avalonia_opengl.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
