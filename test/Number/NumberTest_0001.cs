806597ac086be6d561f734291b739e3daa032df4
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-08-03 03:35:13 +0200

    number unit tests

---
+++ test/Number/NumberTest_0001.cs
@@ -0,0 +1,20 @@
+using Xunit;
+using System.Linq;
+using System;
+
+namespace SearchAThing.Sci.Tests
+{
+    public partial class NumberTests
+    {
+
+        [Fact]
+        public void NumberTest_0001()
+        {             
+            var v = 13.45;
+
+            // use Round(13.45, 3) to get precise 13.45
+            Assert.False((v.MRound(1e-3) - 13.45) < double.Epsilon);
+        }
+
+    }
+}
