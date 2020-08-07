c835880281b03a20389b3b957c78b92b8b637891
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-08-08 01:14:11 +0200

    doc

--- netcore-sci/Number.cs
+++ netcore-sci/Number.cs
@@ -243,21 +243,7 @@ namespace SearchAThing
 
             return wcnt;
         }
-
-        /// <summary>
-        /// Measure percent difference between given two numbers;
-        /// return double.NaN if only one of two numbers are 0;
-        /// 0 if both two given numbers are 0.
-        /// 
-        /// Given f = PercentDifference(x, y)
-        ///   m = Min(x, y)
-        ///   M = Max(x, y)        
-        ///   a = Min(Abs(x), Abs(y))
-        /// 
-        /// returned value f satisfy follow condition        
-        ///   M(m, a, f) = m + a * f
-        /// </summary>
-
+            
         /// <summary>
         /// Test two numbers for similarity; the factor
         /// of similarity f = (max(x,y)-min(x,y)) / min(abs(x), abs(y)).
