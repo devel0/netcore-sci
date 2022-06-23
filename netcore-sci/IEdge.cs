92045f97ea5c2427e5b8216e9953c4e58cda10d1
Lorenzo Delana <lorenzo.delana@gmail.com>
2022-06-23 16:54:58 +0200

    doc

--- netcore-sci/IEdge.cs
+++ netcore-sci/IEdge.cs
@@ -19,6 +19,10 @@ namespace SearchAThing
 
     public interface IEdge
     {
+
+        /// <summary>
+        /// type of edge
+        /// </summary>        
         EdgeType EdgeType { get; }
         
         /// <summary>
@@ -26,18 +30,39 @@ namespace SearchAThing
         /// </summary>        
         bool Sense { get; }
 
+        /// <summary>
+        /// (side effect)
+        /// Toggle Sense flag so that SGeomFrom, SGeomTo equals to GeomFrom, GeomTo (Sense:true)
+        /// or GeomTo, GeomFrom (Sense:false)
+        /// </summary>
         void ToggleSense();
 
+        /// <summary>
+        /// GeomFrom (Sense:true) or GeomTo (Sense:false)
+        /// </summary>
         Vector3D SGeomFrom { get; }
 
+        /// <summary>
+        /// GeomTo (Sense:true) or GeomFrom (Sense:false)
+        /// </summary>
+        /// <value></value>
         Vector3D SGeomTo { get; }
 
         Vector3D MidPoint { get; }
 
+        /// <summary>
+        /// States if edge contains given point on its perimeter
+        /// </summary>        
         bool EdgeContainsPoint(double tol, Vector3D pt);
 
+        /// <summary>
+        /// Split this edge into one or more geometries based on given breaks.        
+        /// </summary>        
         IEnumerable<Geometry> Split(double tol, IEnumerable<Vector3D> breaks);
 
+        /// <summary>
+        /// Retrieve corresponding dxf entity to this edge
+        /// </summary>        
         netDxf.Entities.EntityObject DxfEntity { get; }
 
     }
