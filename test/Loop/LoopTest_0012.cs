105f0faf08580377bcb3943dc3f01295d4aad554
Lorenzo Delana <lorenzo.delana@gmail.com>
2022-07-02 00:33:32 +0200

    unit test for arc3d equals fixture

---
+++ test/Loop/LoopTest_0012.cs
@@ -0,0 +1,46 @@
+using Xunit;
+using System.Linq;
+using System;
+using netDxf;
+using netDxf.Entities;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+
+namespace SearchAThing.Sci.Tests
+{
+    public partial class LoopTests
+    {
+
+        [Fact]
+        public void LoopTest_0012()
+        {
+            var dxf = netDxf.DxfDocument.Load(
+                System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Loop/LoopTest_0012.dxf"));
+
+            DxfDocument? outdxf = null;
+            outdxf = new DxfDocument();
+
+            var tol = 1e-8;
+
+            var loopGreen = dxf.LwPolylines.First(w => w.Color.Index == AciColor.Green.Index).ToLoop(tol);
+            var loopYellow = dxf.LwPolylines.First(w => w.Color.Index == AciColor.Yellow.Index).ToLoop(tol);            
+
+            outdxf?.AddEntity(loopGreen.DxfEntity(tol).Set(x => x.SetColor(AciColor.Green)));
+            outdxf?.AddEntity(loopYellow.DxfEntity(tol).Set(x => x.SetColor(AciColor.Yellow)));            
+
+            var yellowIntersectGreen = loopYellow.Intersect(tol, loopGreen, outdxf).ToList();            
+
+            Assert.True(yellowIntersectGreen.Count == 1);
+            
+            if (outdxf != null)
+            {
+                outdxf.DrawingVariables.PdMode = netDxf.Header.PointShape.CircleCross;
+                outdxf.Viewport.ShowGrid = false;
+                outdxf.Save(System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "out.dxf"));
+            }
+        }
+
+    }
+
+}
