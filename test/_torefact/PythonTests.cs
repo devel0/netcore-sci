3e061679bfdb7e6796f722a9ab2fb2167943085b
Lorenzo Delana <lorenzo.delana@gmail.com>
2022-10-22 03:41:35 +0200

    fix pythonpipe unit tests

--- test/_torefact/PythonTests.cs
+++ test/_torefact/PythonTests.cs
@@ -33,32 +33,32 @@ namespace SearchAThing.Sci.Tests
         }
 
         [Fact]
-        public void Test0()
+        public async void Test0()
         {
             var strwr = new StringWrapper("print(1+2)");            
-            var res = pipe.Exec(strwr).str;
+            var res = (await pipe.Exec(strwr)).str;
             var reslines = res.Lines().ToArray();
             Assert.True(reslines.Length == 1 && reslines[0] == "3");
         }
 
         [Fact]
-        public void Test1()
+        public async void Test1()
         {
             var strwr = new StringWrapper("print(1+2)\r\nprint(3+5)");            
-            var res = pipe.Exec(strwr).str;
+            var res = (await pipe.Exec(strwr)).str;
             var reslines = res.Lines().ToArray();
             Assert.True(reslines.Length == 2 && reslines[0] == "3" && reslines[1] == "8");
         }
 
         [SkippableFact]
-        public void Test2()
+        public async void Test2()
         {
             Skip.IfNot(has_sympy_library);
 
             var strwr = new StringWrapper(@"x = symbols('x')
 print(integrate(x**2 * cos(x), x))
 print(integrate(x**2 * cos(x), (x, 0, pi/2)))");            
-            var res = sympyPipe.Exec(strwr).str;
+            var res = (await sympyPipe.Exec(strwr)).str;
             var reslines = res.Lines().ToArray();
             Assert.True(reslines.Length == 2 && reslines[0] == "x**2*sin(x) + 2*x*cos(x) - 2*sin(x)" && reslines[1] == "-2 + pi**2/4");
         }
