7400719f0a466270316932b794b9997383d6e573
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-16 01:36:11 +0200

    doc

--- docs/html/search/all_14.js
+++ docs/html/search/all_14.js
@@ -4,5 +4,7 @@ var searchData=
   ['walkedgestopconditiondelegate_1',['WalkEdgeStopConditionDelegate',['../class_search_a_thing_1_1_sci_toolkit.html#adda1fcaa81903d8a0be4fccb3aa79260',1,'SearchAThing::SciToolkit']]],
   ['wcs_2',['WCS',['../class_search_a_thing_1_1_coordinate_system3_d.html#adb718d233dbeb30c04d6afc131fd6b37',1,'SearchAThing::CoordinateSystem3D']]],
   ['weighteddistribution_3',['WeightedDistribution',['../class_search_a_thing_1_1_sci_ext.html#a858dc00c8346af08cef5e2c86e4f0826',1,'SearchAThing::SciExt']]],
-  ['writejson_4',['WriteJson',['../class_search_a_thing_1_1_sci_toolkit_1_1_unit_json_converter.html#ac1df09a660752ecf0fa3d09616be468a',1,'SearchAThing::SciToolkit::UnitJsonConverter']]]
+  ['win32_5fmax_5fstring_5flen_4',['win32_max_string_len',['../class_search_a_thing_1_1_python_pipe.html#a791055a0701e79b43dab208b4a7fa014',1,'SearchAThing::PythonPipe']]],
+  ['win32_5fstring_5flen_5fsafe_5',['win32_string_len_safe',['../class_search_a_thing_1_1_python_pipe.html#ae50e24171ae02835b3a5271f68ff2b6b',1,'SearchAThing::PythonPipe']]],
+  ['writejson_6',['WriteJson',['../class_search_a_thing_1_1_sci_toolkit_1_1_unit_json_converter.html#ac1df09a660752ecf0fa3d09616be468a',1,'SearchAThing::SciToolkit::UnitJsonConverter']]]
 ];
--- docs/html/search/variables_b.js
+++ docs/html/search/variables_b.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['wcs_0',['WCS',['../class_search_a_thing_1_1_coordinate_system3_d.html#adb718d233dbeb30c04d6afc131fd6b37',1,'SearchAThing::CoordinateSystem3D']]]
+  ['wcs_0',['WCS',['../class_search_a_thing_1_1_coordinate_system3_d.html#adb718d233dbeb30c04d6afc131fd6b37',1,'SearchAThing::CoordinateSystem3D']]],
+  ['win32_5fmax_5fstring_5flen_1',['win32_max_string_len',['../class_search_a_thing_1_1_python_pipe.html#a791055a0701e79b43dab208b4a7fa014',1,'SearchAThing::PythonPipe']]],
+  ['win32_5fstring_5flen_5fsafe_2',['win32_string_len_safe',['../class_search_a_thing_1_1_python_pipe.html#ae50e24171ae02835b3a5271f68ff2b6b',1,'SearchAThing::PythonPipe']]]
 ];
