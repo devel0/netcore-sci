6caf1eeea805a4b82534544fcf7b5096fed6f9b7
Lorenzo Delana <lorenzo.delana@gmail.com>
2023-04-16 02:57:47 +0200

    doc

--- docs/html/search/classes_d.js
+++ docs/html/search/classes_d.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['toolki_0',['Toolki',['../class_search_a_thing_1_1_sci_1_1_toolki.html',1,'SearchAThing::Sci']]],
-  ['toolkit_1',['Toolkit',['../class_search_a_thing_1_1_sci_1_1_toolkit.html',1,'SearchAThing::Sci']]],
-  ['transform3d_2',['Transform3D',['../class_search_a_thing_1_1_sci_1_1_transform3_d.html',1,'SearchAThing::Sci']]],
-  ['triangle3d_3',['Triangle3D',['../struct_search_a_thing_1_1_sci_1_1_triangle3_d.html',1,'SearchAThing::Sci']]]
+  ['toolkit_0',['Toolkit',['../class_search_a_thing_1_1_sci_1_1_toolkit.html',1,'SearchAThing::Sci']]],
+  ['transform3d_1',['Transform3D',['../class_search_a_thing_1_1_sci_1_1_transform3_d.html',1,'SearchAThing::Sci']]],
+  ['triangle3d_2',['Triangle3D',['../struct_search_a_thing_1_1_sci_1_1_triangle3_d.html',1,'SearchAThing::Sci']]]
 ];
